ffd025c0fc8eb139950e1b7d6bd27e612eb4378a
Robert Bragg <robert@sixbynine.org>
2015-03-13 22:10:47 +0000

    i965: Implement INTEL_performance_query extension
    This adds a bare-bones backend for the INTEL_performance_query extension
    that exposes the pipeline statistics on gen 6 and 7 hardware.
    
    Although this could be considered redundant given that the same
    statistics are now available via query objects, they are a simple
    starting point for this extension and it's expected to be convenient for
    tools wanting to have a single go to api to introspect what performance
    counters are available, along with names, descriptions and semantic/data
    types.
    
    This code is derived from Kenneth Graunke's work, temporarily removed
    while the frontend and backend interface were reworked.
    
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- src/mesa/drivers/dri/i965/brw_context.h
+++ src/mesa/drivers/dri/i965/brw_context.h
@@ -735,6 +735,21 @@ struct shader_times;
 
 struct brw_l3_config;
 
+enum brw_query_kind {
+   PIPELINE_STATS
+};
+
+struct brw_perf_query
+{
+   enum brw_query_kind kind;
+   const char *name;
+   struct brw_perf_query_counter *counters;
+   int n_counters;
+   size_t data_size;
+};
+
+#define MAX_PERF_QUERIES 3
+
 /**
  * brw_context is derived from gl_context.
  */
@@ -1231,6 +1246,13 @@ struct brw_context
       bool supported;
    } predicate;
 
+   struct {
+      struct brw_perf_query queries[MAX_PERF_QUERIES];
+      int n_queries;
+
+      int n_active_pipeline_stats_queries;
+   } perfquery;
+
    int num_atoms[BRW_NUM_PIPELINES];
    const struct brw_tracked_state render_atoms[76];
    const struct brw_tracked_state compute_atoms[11];
@@ -1539,6 +1561,10 @@ void brw_upload_image_surfaces(struct brw_context *brw,
 bool brw_render_target_supported(struct brw_context *brw,
                                  struct gl_renderbuffer *rb);
 uint32_t brw_depth_format(struct brw_context *brw, mesa_format format);
+
+/* brw_performance_query.c */
+void brw_init_performance_queries(struct brw_context *brw);
+void brw_dump_perf_queries(struct brw_context *brw);
 
 /* intel_buffer_objects.c */
 int brw_bo_map(struct brw_context *brw, drm_intel_bo *bo, int write_enable,
